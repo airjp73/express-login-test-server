40e6250289a5e5a3f855dc0b62d10960410ad7b1
AaronP <aaron.pettengill.violin@gmail.com>
2017-12-15 13:55:42 -0500

    Fixed tests

--- src/authRouter.spec.js
+++ src/authRouter.spec.js
@@ -2,12 +2,27 @@
 
 var chai = require('chai')
 var expect = chai.expect
+var sinon = require('sinon')
 
 var express = require('express')
+
 var authRouter = require('./authRouter.js')
+var router = {
+  route: sinon.stub(),
+  get: sinon.stub()
+}
+router.route.returns(router)
+router.get.returns(router)
+
+var mockExpressLogin = {
+  createAuthRouter: sinon.stub().returns(router)
+}
 
 describe("authRouter", () => {
-  it("should export an express router", () => {
-    expect(Object.getPrototypeOf(authRouter)).to.equal(express.Router)
+  it("should call createAuthRouter on arg and return result", () => {
+    var test = authRouter(mockExpressLogin)
+
+    sinon.assert.calledOnce(mockExpressLogin.createAuthRouter)
+    expect(test).to.equal(router)
   })
 })
